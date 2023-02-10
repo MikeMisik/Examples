deb662423a052dbfa3a3cacf133692c58acba7cf
Mike Misik <mike-misik@yandex.ru>
2023-02-10 22:35:35 +0300

    added seminar1

---
+++ Examples/seminar1/Task001/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Введите число ");
+string? answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+Console.WriteLine(number * number);
---
+++ Examples/seminar1/Task003/Program.cs
@@ -0,0 +1,39 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+ 
+if (daynumber == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (daynumber == 2)
+{
+    Console.WriteLine("вторник");
+}
+else if (daynumber == 3)
+{
+    Console.WriteLine("среда");
+}
+else if (daynumber == 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (daynumber == 5)
+{
+    Console.WriteLine("пятница, БУХАЕМ!!!");
+}
+else if (daynumber == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (daynumber == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+else 
+{
+    Console.WriteLine("нет такого дня недели");
+}
