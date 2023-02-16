fa8b9c9318a7b86256e6c758c2528c2bfb6fdad1
Mike Misik <mike-misik@yandex.ru>
2023-02-16 19:02:14 +0300

    HomeWorkSeminar2_01 added

---
+++ Examples/seminar2/HomeWorkSeminar2/HomeWorkSeminar2_01/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 10: Напишите программу, которая 
+// принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число ");
+string? answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+int min = 100;
+int max = 999;
+
+if (number < min || number > max)
+{
+    Console.WriteLine("Введённое число не является трёхзначным ");
+}
+else 
+{
+    int digit1 = (number / 10) % 10;
+    // int digit2 = digit1 % 10;
+    Console.WriteLine(digit1);
+}
+
