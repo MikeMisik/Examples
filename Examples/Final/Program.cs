3cbed6692c26132393273aafaa80da6568e31d58
Mike Misik <mike-misik@yandex.ru>
2023-03-26 14:15:00 +0300

    README added

---
+++ Examples/Final/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая из имеющищегося массива строк
+// формирует массив из строк, длина которых меньше либо равна 3.
+
+
+string[] newArray = { "hello", "2", "world", ":-)" };
+Console.WriteLine(string.Join(", ", newArray ));
+for (int i = 0; i < newArray.Length; i++)
+{
+    if newArray[i] 
+}
+
